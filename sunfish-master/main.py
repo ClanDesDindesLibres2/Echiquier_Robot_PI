28ee45b7715fdd5aaaae91a7cc99eb0bbb5db18b
berg0401 <Gabriel.Bertrand@USherbrooke.ca>
2022-03-09 16:04:55 -0500

    comm openCR_pi

---
+++ sunfish-master/main.py
@@ -0,0 +1,11 @@
+import serial
+
+ser = serial.Serial('COM4', 9600)
+s = [0]
+while True:
+	message = str(ser.readline())
+	print(message)
+	#initiale = str(message[2:][:-8], 16)
+	finale = int(message[5:][:-5], 16)
+	print(finale)
+	#print(initiale, finale)
---
+++ sunfish-master/test_communication.py
@@ -0,0 +1,5 @@
+import OpenCR_comm
+
+openCR_comm = OpenCR_comm.communicationOPENCR('COM3')
+openCR_comm.sendData(12,14,0)
+
