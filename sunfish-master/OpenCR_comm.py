3acfb76adfb3fd0051ab9d173881175e6c5bed9c
berg0401 <Gabriel.Bertrand@USherbrooke.ca>
2022-04-06 09:36:24 -0400

    comm read

--- sunfish-master/OpenCR_comm.py
+++ sunfish-master/OpenCR_comm.py
@@ -20,9 +20,32 @@ class communicationOPENCR:
                     if  OpenCR.inWaiting()>0: 
                         answer=OpenCR.readline()
                         OpenCR.flushInput() #remove data after reading
+                        print(answer)
+                        done = int(answer)
+                        if done>=1:
+                            done = done+1
+                            print(done)
+
+
+
+                        
                 except KeyboardInterrupt:
                     print("KeyboardInterrupt has been caught.")
+    """
+        def readData(self):
+        with serial.Serial(self.port, 9600, timeout=1) as OpenCR:
+            while True:
+                if OpenCR.in_waiting >0:
+                    done = OpenCR.readline().decode('utf-8').rstrip
+                    print(done)
+                    break
+    """
+
+
 
 if __name__ == '__main__':
     openCR_comm = communicationOPENCR('COM3')
     openCR_comm.sendData(12,14,0)
+    yo = 4
+    print(yo)
+    ##openCR_comm.readData()
